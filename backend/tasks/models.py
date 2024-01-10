3c78ac14979bb59f922d39cf5fead03f51513803
BigSecret1 <dk2020mailaddress@gmail.com>
2024-01-10 09:03:32 +0530

    add postgresql implementation in tasks app view to execute create task query using tasks/create endpoint.

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    subtasks = models.JSONField(default=list)  # Assuming subtasks are stored as JSON
+    due_date = models.DateField(null=True, blank=True)
+    comments = models.TextField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    task_status = models.CharField(max_length=20, default='To Do')
+
--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -4,10 +4,31 @@ from django.views.decorators.csrf import csrf_exempt
 from django.db import connection
 import json
 
+
 @csrf_exempt
 def create_task(request):
-    if request.method == 'GET':
-        return JsonResponse({'name': "My name is Alice !"})
+    if request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+
+        with connection.cursor() as cursor:
+            cursor.execute(
+                    '''
+                INSERT INTO tasks(title, subtasks, due_date, comments, description, task_status)
+                VALUES (%s, %s, %s, %s, %s, %s)
+                RETURNING id;
+                ''',
+                [
+                    data.get('title', ''),
+                    data.get('subtasks', []),
+                    data.get('due_date', None),
+                    data.get('comments', ''),
+                    data.get('description', ''),
+                    data.get('task_status', 'To Do'),
+                    ]
+                )
+
+            task_id = cursor.fetchone()[0]
+
+        return JsonResponse({'task_id': task_id})
     else:
         return JsonResponse({'error': 'Only POST requests are allowed.'})
-
