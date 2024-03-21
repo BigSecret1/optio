8a0074e5c5ec366bdbf931bb261bfc6abe8134f3
BigSecret1 <dk2020mailaddress@gmail.com>
2024-03-21 23:54:16 +0530

    [create-task-api] feat[api]: add create task api wit unit test cases.

--- backend/tasks/tests.py
+++ backend/tasks/tests.py
@@ -1,34 +1,85 @@
 from django.test import TestCase, Client
+import psycopg2
+from django.db import connection
+from django.http import JsonResponse
+from unittest.mock import patch, ANY, MagicMock
+from unittest import mock
 import json 
 from tasks.views import create_task
 
+from psycopg2.extras import RealDictCursor
 
 
 class CreateTaskViewTestCase(TestCase):
-
+    
     def setUp(self):
         self.client = Client()
 
-
-    def test_invalid_request_status_code(self):
-
+    def test_wrong_request_method(self):
+        expected_result = {
+        "id": 33,
+        "title": "Complete Project"
+        }
+        request_data = {
+        "title": "Request from tests",
+        "subtasks": ["Subtask1", "Subtask2"],
+        "due_date": "2024-02-04",
+        "comments": ["This is the first test comment", "This is the second comment"],
+        "description": "This is test description",
+        "task_status": "In Progress"
+        }
+        request_body = json.dumps(request_data)
+        print(request_body)
         response = self.client.get('/tasks/create/')
 
-        # Assert the status code of response(Bad Request)
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code,400)
+        self.assertEqual(response.json(),{'error': 'Only POST requests are allowed.'})
 
 
-    def test_invalid_request_error_message(self):
+    def test_create_task_success(self):
 
-        response = self.client.get('/tasks/create/')
+        expected_result = {
+            "id": 33,
+            "title": "Complete Project"
+        }
+        request_data = {
+            "title": "Request from tests",
+            "subtasks": ["Subtask1", "Subtask2"],
+            "due_date": "2024-02-04",
+            "comments": ["This is the first test comment", "This is the second comment"],
+            "description": "This is test description",
+            "task_status": "In Progress"
+        }
+        request_body = json.dumps(request_data)
+        print(request_body)
+        response = self.client.post('/tasks/create/', data=request_body, content_type="application/json")
+
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_wrong_post(self):
         
-        print("RESPONSE: ", response)
+        expected_result = {
+            "id": 33,
+            "title": "Complete Project"
+        }
+        request_data = {
+            "title": "Request from tests",
+            "subtasks": ["Subtask1", "Subtask2"],
+            "due_date": "2024-02-04444",
+            "comments": ["This is the first test comment", "This is the second comment"],
+            "description": "This is test description",
+            "task_status": "In Progress"
+        }
+        request_body = json.dumps(request_data)
+        print(request_body)
+        response = self.client.post('/tasks/create/', data=request_body, content_type="application/json")
 
-        error = response.json()
-        error_message = error.get("error")
+        self.assertEqual(response.status_code, 500)
+        self.assertEqual(response.json(),{'error': 'wrong request body'})
 
-        # Assert the expected response content
-        self.assertEqual({'error': error_message}, {'error': 'Only POST requests are allowed.'})
+
+
 
 
 
--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,45 +1,89 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse 
 from django.views.decorators.csrf import csrf_exempt
 from django.db import connection
 import json
 import pandas as pd
 from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework import status
+
+
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+
+
+
+
+import logging
+
+# Configure the logging module (usually done at the beginning of your script)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def create_connection():
+    while True:
+        try:
+            conn = psycopg2.connect(
+                    host='localhost',
+                    database='todoler',
+                    user='dinesh',
+                    password='neverworry'
+                    )
+            cur = conn.cursor(cursor_factory=RealDictCursor)
+            print("Database connection was successful")
+            return conn, cur
+        except Exception as err:
+            print("Error when trying to connect to PostgreSQL DB -> ", err)
+            time.sleep(2)
+
+def close_connection(conn, cur):
+    cur.close()
+    conn.close()
+    print("Database connection closed")
 
 
 @csrf_exempt
 def create_task(request):
+
     if request.method == 'POST':
         try:
             data = json.loads(request.body.decode('utf-8'))
 
-            with connection.cursor() as cursor:
-                cursor.execute(
-                        '''
-                    INSERT INTO tasks(title, subtasks, due_date, comments, description, task_status)
-                    VALUES (%s, %s, %s, %s, %s, %s)
-                    RETURNING *;
-                    ''',
-                    [
-                        data.get('title', ''),
-                        data.get('subtasks', []),
-                        data.get('due_date', None),
-                        data.get('comments', ''),
-                        data.get('description', ''),
-                        data.get('task_status', 'To Do'),
-                        ]
-                    )
+            conn, cur = create_connection() 
+            query = """ 
+                INSERT INTO tasks(title, subtasks, due_date, comments, description, task_status)
+                VALUES (%s, %s, %s, %s, %s, %s)
+                RETURNING *;
+               """ 
+            logging.info(query)
+            cur.execute(query, (data.get('title', ''),
+                                data.get('subtasks', []),
+                                data.get('due_date', None),
+                                data.get('comments', ''),
+                                data.get('description', ''),
+                                data.get('task_status', 'To Do'),
+                                ))
+            post = cur.fetchone()
+            logging.info(post)
 
-                columns = [col[0] for col in cursor.description]
-                task = cursor.fetchone()
-                task = dict(zip(columns, task))
-                df = pd.DataFrame.from_records([task])
-                task_detail = {}
-                task_detail["id"] = int(df.at[0,"id"])
-                task_detail["title"] = str(df.at[0,"title"])
 
-            return JsonResponse(task_detail, status=200)
-        except exception as e:
-            return JsonResponse({"error": str(e)}, status=500)
+            conn.commit()
+            conn.close()
+            return JsonResponse(post, status=200)
+
+        except Exception as err:
+            #return Response({"error": "wrong request body"},status=status.HTTP_201_CREATED)
+             return JsonResponse({"error": "wrong request body"}, status=500, content_type='application/json')
     else:
         return JsonResponse({'error': 'Only POST requests are allowed.'}, status=400)
+
+
+
+
+
+
+
+
+
