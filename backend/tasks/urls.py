67d3ea30a6bb7d4d34f0abd648aa7b16551d51ff
BigSecret1 <dk2020mailaddress@gmail.com>
2024-03-23 22:46:09 +0530

    [get-task-api] feat[api]: add initial working implementation of get task api.

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import create_task
+from .views import create_task, get_task_by_id
 
 urlpatterns = [
-    path('create/', create_task, name='create_task'),
+    path('create/', create_task, name='create-task'),
+    path('get/<int:task_id>/', get_task_by_id, name='get-task')
+
 ]
 
