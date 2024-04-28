77c9d85ea053a3d91b4b549eaae0d60605287c51
BigSecret1 <dk2020mailaddress@gmail.com>
2024-04-28 14:58:58 +0530

    [delete-task-api] feat[api] add delete task api, it's workking fine.

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import create_task, get_task_by_id, update_task
+from .views import create_task, get_task_by_id, update_task, delete_task
 
 urlpatterns = [
     path('create/', create_task, name='create-task'),
-    path('get/<int:task_id>/', get_task_by_id, name='get-task'),
-    path('update/<int:task_id>/', update_task, name='update-task')
+    path('get/<int:task_id>', get_task_by_id, name='get-task'),
+    path('update/<int:task_id>', update_task, name='update-task'),
+     path('delete/<int:task_id>', delete_task, name='delete-task')
 ]
 
