963291ee2678628a4d22e7e2c9b47e860fd796da
BigSecret1 <dk2020mailaddress@gmail.com>
2024-07-28 22:05:23 +0530

    Add first componenet [Navbar] with basic styling

---
+++ front-end/src/components/Navbar.js
@@ -0,0 +1,29 @@
+
+import React from 'react';
+import './Navbar.css';  // Import the custom CSS file
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg custom-navbar">
+      <a className="navbar-brand" href="#">My App</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="http://your-projects-url.com" target="_blank" rel="noopener noreferrer">Projects</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="http://your-tasks-url.com" target="_blank" rel="noopener noreferrer">Tasks</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="http://your-performance-metrics-url.com" target="_blank" rel="noopener noreferrer">Performance Metrics</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// import bootstrap for the project
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import custom components
+import Navbar from './components/Navbar';  
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <Navbar/>
   </React.StrictMode>
 );
 
