fa7070bbd82ce1b1e31842bc1a4b720c6e0ff05f
BigSecret1 <dk2020mailaddress@gmail.com>
2024-08-10 11:06:35 +0530

    Fix Date invalid issue in Project component

--- front-end/src/components/Projects.js
+++ front-end/src/components/Projects.js
@@ -11,7 +11,7 @@ const Projects = () => {
         method: 'GET',
         headers: {
              'Content-Type': 'application/json',
-             'Authorization': `Bearer ${token}`, // Include the JWT token in the Authorization header
+             'Authorization': `Bearer ${token}`, 
         },
     })
       .then(response => response.json())
@@ -24,6 +24,28 @@ const Projects = () => {
       });
   }, []);
 
+  // Helper function to convert UTC to local time in "yy:dd:mm:H:M" format
+  const convertToLocalTime = (utcDateStr) => {
+    console.log("RECEIVED UTC TIME :", utcDateStr);
+    const utcDate = new Date(utcDateStr);
+    const localDay = utcDate.getDate();
+    const localMonth = utcDate.getMonth() + 1; // Months are zero-based
+    const localYear = utcDate.getFullYear().toString(); // Get last two digits of the year
+    const localHours = utcDate.getHours(); // Local hours
+    const localMinutes = utcDate.getMinutes(); // Local minutes
+
+    // Format the date and time components
+    const formattedDay = localDay.toString().padStart(2, '0');
+    const formattedMonth = localMonth.toString().padStart(2, '0');
+    const formattedYear = localYear.toString().padStart(4, '0');
+    const formattedHours = localHours.toString().padStart(2, '0');
+    const formattedMinutes = localMinutes.toString().padStart(2, '0');
+
+    const localTime = `${formattedYear}-${formattedDay}-${formattedMonth}${' '}${formattedHours}:${formattedMinutes}`;
+    console.log("CONVERTED TIME :", localTime)
+    return localTime
+  };
+
   return (
     <div className="projects-container">
       {projects.map((project, index) => (
@@ -31,8 +53,10 @@ const Projects = () => {
           <h3 className="project-name">{project.name}</h3>
           <p className="project-description">{project.description}</p>
           <div className="project-info">
-            <span className="project-stars">⭐ {project.stars}</span>
-            <span className="project-updated">Updated {new Date(project.project_updated).toLocaleString()}</span>
+            <span className="project-stars">⭐  Starred</span>
+            <span className="project-updated">
+              Modified : {convertToLocalTime(project.last_updated)}
+            </span>
           </div>
         </div>
       ))}
