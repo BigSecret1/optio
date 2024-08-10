992cec0904ad32788e0c124734ab3da7ca9b6321
BigSecret1 <dk2020mailaddress@gmail.com>
2024-08-10 11:10:09 +0530

    Add misc confi changes realted to jupyter, virutal env etc.

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -159,7 +159,7 @@ AUTH_USER_MODEL = 'profiles.CustomUser'
 # For JWT token
 from datetime import timedelta
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=180),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
