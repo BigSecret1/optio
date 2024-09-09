ba3f2b1369a481eacec6d99e8535109632218f6a
BigSecret1 <dk2020mailaddress@gmail.com>
2024-09-09 08:27:10 +0530

    Change layout dimension, still need improvment though.

--- front-end/src/components/ShowTask.js
+++ front-end/src/components/ShowTask.js
@@ -31,65 +31,61 @@ function ShowTask() {
                 {"I'm a flexbox container that uses flex!"}
             </Box>
 
-
-
-            <div className="descriptionSubTasks">
-                <div className="description">
-                    <Box
-                        sx={[
-                            (theme) => ({
-                                display: 'flext',
-                                m: 1,
-                                width: '100%',
-                                p: 1,
-                                height: '100px',
-                                bgcolor: '#fff',
-                                color: 'grey.800',
-                                border: '1px solid',
-                                borderColor: 'grey.300',
-                                borderRadius: 2,
-                                fontSize: '0.875rem',
-                                fontWeight: '700',
-                                ...theme.applyStyles('dark', {
-                                    bgcolor: '#101010',
-                                    color: 'grey.300',
-                                    borderColor: 'grey.800',
-                                }),
+            <div className="description">
+                <Box
+                    sx={[
+                        (theme) => ({
+                            display: 'flext',
+                            m: 1,
+                            width: '100%',
+                            p: 1,
+                            height: '100%',
+                            bgcolor: '#fff',
+                            color: 'grey.800',
+                            border: '1px solid',
+                            borderColor: 'grey.300',
+                            borderRadius: 2,
+                            fontSize: '0.875rem',
+                            fontWeight: '700',
+                            ...theme.applyStyles('dark', {
+                                bgcolor: '#101010',
+                                color: 'grey.300',
+                                borderColor: 'grey.800',
                             }),
-                        ]}
-                    >
-                        {"I'm a flexbox container that uses flex!"}
-                    </Box>
-                </div>
+                        }),
+                    ]}
+                >
+                    {"I'm a flexbox container that uses flex!"}
+                </Box>
+            </div>
 
-                <div className="subTasks">
-                    <Box
-                        sx={[
-                            (theme) => ({
-                                display: 'flext',
-                                m: 1,
-                                width: '50%',
-                                p: 1,
-                                height: '80%',
-                                width: '100%',
-                                bgcolor: '#fff',
-                                color: 'grey.800',
-                                border: '1px solid',
-                                borderColor: 'grey.300',
-                                borderRadius: 2,
-                                fontSize: '0.875rem',
-                                fontWeight: '700',
-                                ...theme.applyStyles('dark', {
-                                    bgcolor: '#101010',
-                                    color: 'grey.300',
-                                    borderColor: 'grey.800',
-                                }),
+            <div className="subTasks">
+                <Box
+                    sx={[
+                        (theme) => ({
+                            display: 'flext',
+                            m: 1,
+                            width: '50%',
+                            p: 1,
+                            height: '100%',
+                            width: '100%',
+                            bgcolor: '#fff',
+                            color: 'grey.800',
+                            border: '1px solid',
+                            borderColor: 'grey.300',
+                            borderRadius: 2,
+                            fontSize: '0.875rem',
+                            fontWeight: '700',
+                            ...theme.applyStyles('dark', {
+                                bgcolor: '#101010',
+                                color: 'grey.300',
+                                borderColor: 'grey.800',
                             }),
-                        ]}
-                    >
-                        {"Ehse are for subtasks"}
-                    </Box>
-                </div>
+                        }),
+                    ]}
+                >
+                    {"Ehse are for subtasks"}
+                </Box>
             </div>
 
             <div className="comments">
@@ -99,7 +95,7 @@ function ShowTask() {
                             display: 'flext',
                             m: 1,
                             width: '100%',
-                            height: '90%',
+                            height: '100%',
                             p: 1,
                             bgcolor: '#fff',
                             color: 'grey.800',
