ea326ea038ff36764e313cc6f0db4e945ac04ccf
BigSecret1 <dk2020mailaddress@gmail.com>
2024-09-10 09:08:11 +0530

    Change show task layout , make it simple.

--- front-end/src/components/ShowTask.js
+++ front-end/src/components/ShowTask.js
@@ -1,24 +1,134 @@
-import React from "react";
+import * as React from 'react';
 import Box from '@mui/material/Box';
-import './ShowTask.css'
 
-
-function ShowTask() {
+export default function ShowTasks() {
     return (
-        <>
-            <div class="container">
-                <div class="column column-1">
-                    <div class="box small-box"></div>
-                    <div class="box small-box"></div>
+        <div style={{ width: '100%' }}>
+            <Box
+                sx={[
+                    (theme) => ({
+                        display: 'flex',
+                        m: 1,
+                        p: 1,
+                        height: '10vh',
+                        bgcolor: '#304971',
+                        color: 'grey.800',
+                        border: '1px solid',
+                        borderColor: 'grey.300',
+                        borderRadius: 2,
+                        fontSize: '0.875rem',
+                        fontWeight: '700',
+                        ...theme.applyStyles('dark', {
+                            bgcolor: '#101010',
+                            color: 'grey.300',
+                            borderColor: 'grey.800',
+                        }),
+                    }),
+                ]}
+            >
+                <h3>Finish the front end task layout in this week end to end</h3>
+            </Box>
+            <Box
+                sx={[
+                    (theme) => ({
+                        display: 'flex',
+                        m: 1,
+                        p: 1,
+                        height: '30vh',
+                        bgcolor: '#304971',
+                        color: 'grey.800',
+                        border: '1px solid',
+                        borderColor: 'grey.300',
+                        borderRadius: 2,
+                        fontSize: '0.875rem',
+                        fontWeight: '700',
+                        ...theme.applyStyles('dark', {
+                            bgcolor: '#101010',
+                            color: 'grey.300',
+                            borderColor: 'grey.800',
+                        }),
+                    }),
+                ]}
+            >
+                <h3>Description</h3>
+            </Box>
 
+            <Box
+                sx={[
+                    (theme) => ({
+                        display: 'flex',
+                        m: 1,
+                        p: 1,
+                        height: '40vh',
+                        overflow: 'auto',
+                        bgcolor: '#304971',
+                        color: 'grey.800',
+                        border: '1px solid',
+                        borderColor: 'grey.300',
+                        borderRadius: 2,
+                        fontSize: '0.875rem',
+                        fontWeight: '700',
+                        ...theme.applyStyles('dark', {
+                            bgcolor: '#101010',
+                            color: 'grey.300',
+                            borderColor: 'grey.800',
+                        }),
+                    }),
+                ]}
+            >
+                <h3>SubTasks</h3>
+                <div className='subTasks'>
+                    <div className='taskItem'>
+                        <input type='checkbox' />
+                        <h6>Lay down all elementin one row</h6>
+                    </div>
+                    <div className='taskItem'>
+                        <input type='checkbox' />
+                        <h6>Lay down all elementin one row</h6>
+                    </div>
+                    <div className='taskItem'>
+                        <input type='checkbox' />
+                        <h6>Lay down all elementin one row</h6>
+                    </div>
+                    <div className='taskItem'>
+                        <input type='checkbox' />
+                        <h6>Lay down all elementin one row</h6>
+                    </div>
+                    <div className='taskItem'>
+                        <input type='checkbox' />
+                        <h6>Lay down all elementin one row</h6>
+                    </div>
                 </div>
-                <div class="column column-2">
-                    <div class="box big-box"></div>
-                </div>
-            </div>
+            </Box>
 
-        </>
+            <Box
+                sx={[
+                    (theme) => ({
+                        display: 'flex',
+                        m: 1,
+                        p: 1,
+                        height: '50vh',
+                        overflow: 'auto',
+                        bgcolor: '#304971',
+                        color: 'grey.800',
+                        border: '1px solid',
+                        borderColor: 'grey.300',
+                        borderRadius: 2,
+                        fontSize: '0.875rem',
+                        fontWeight: '700',
+                        ...theme.applyStyles('dark', {
+                            bgcolor: '#101010',
+                            color: 'grey.300',
+                            borderColor: 'grey.800',
+                        }),
+                    }),
+                ]}
+            >
+                <h3>Comments</h3>
+                <div className='commentBox'>
+                    <textarea rows="10" cols="120"></textarea>
+                </div>
+            </Box>
+        </div>
     );
 }
-
-export default ShowTask;
