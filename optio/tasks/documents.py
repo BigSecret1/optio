c841cebc88d834da563dc757b5de3041a1e0eb1a
BigSecret1 <dk2020mailaddress@gmail.com>
2025-01-15 21:16:21 +0530

    Add new mappings in tasks search documents module and delete and build new index in ES.

--- optio/tasks/documents.py
+++ optio/tasks/documents.py
@@ -1,4 +1,4 @@
-from django_elasticsearch_dsl import Document
+from django_elasticsearch_dsl import Document, Completion, Text
 from django_elasticsearch_dsl.registries import registry
 
 from optio.tasks.models import Task
@@ -6,6 +6,9 @@ from optio.tasks.models import Task
 
 @registry.register_document
 class TaskDocument(Document):
+    task_title = Text(analyzer = "standard")
+    task_title_suggest = Completion()
+
     class Index:
         name = "tasks_index"
 
