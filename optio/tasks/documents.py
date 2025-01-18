c453d74edb59631c07800c27b22d819cb0f7b87d
BigSecret1 <dk2020mailaddress@gmail.com>
2025-01-18 17:04:44 +0530

    Fix documents file issue and make custom field work .

--- optio/tasks/documents.py
+++ optio/tasks/documents.py
@@ -1,20 +1,30 @@
-from django_elasticsearch_dsl import Document, Completion, Text
+from django_elasticsearch_dsl import Document, fields, Completion, Text
 from django_elasticsearch_dsl.registries import registry
 
 from optio.tasks.models import Task
+import logging
 
 
 @registry.register_document
 class TaskDocument(Document):
-    task_title = Text(analyzer = "standard")
-    task_title_suggest = Completion()
+    title_suggest = fields.CompletionField()
 
     class Index:
         name = "tasks_index"
 
+        settings = {'number_of_shards': 1,
+                    'number_of_replicas': 0}
+
     class Django:
         model = Task
-        fields = [
-            "title",
-            "task_status"
-        ]
+        fields = ["title", "task_status"]
+
+    def prepare_title_suggest(self, instance):
+        """
+        Generate the data for the `title_suggest` field.
+        """
+        logging.info(f"Generating title suggestions for: {instance.title}")
+        return {
+            "input": instance.title.split() if instance.title else [],
+            "weight": 1,
+        }
