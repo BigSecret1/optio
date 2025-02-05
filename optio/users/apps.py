755ba1ac090e6264fd057a574985410c40dbca05
BigSecret1 <dk2020mailaddress@gmail.com>
2025-02-05 23:28:05 +0530

    Create groups from signal trigger post migration.

--- optio/users/apps.py
+++ optio/users/apps.py
@@ -1,7 +1,12 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
 
 class ProfilesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = "optio.users"
     app_label = "optiousers"
+
+    def ready(self):
+        from optio.users.signals import create_groups
+        post_migrate.connect(create_groups, sender=self)
--- optio/users/signals.py
+++ optio/users/signals.py
@@ -5,4 +5,8 @@ from django.contrib.auth.models import Group
 
 @receiver(post_migrate)
 def create_groups(sender, **kwargs):
-    pass
+    if sender.name == 'optio.users':
+        group_names = ["Admin", "Manager", "Viewer"]
+
+        for group_name in group_names:
+            Group.objects.get_or_create(name=group_name)
