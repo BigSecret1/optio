5e36d56efee1439a938146b76ce1a124ef945b07
BigSecret1 <dk2020mailaddress@gmail.com>
2025-02-08 10:44:08 +0530

    Refactor.

--- optio/users/signals.py
+++ optio/users/signals.py
@@ -2,12 +2,12 @@ from django.contrib.contenttypes.models import ContentType
 from django.db.models.signals import post_migrate
 from django.contrib.auth.models import Group, Permission
 from django.dispatch import receiver
-from django.contrib.auth.models import Group
 from django.apps import apps
+from django.db.models import Model
 
 from optio.permissions_mapping import APPS_PERMISSIONS
 
-from typing import Dict, List
+from typing import Dict, List, Iterator
 
 
 @receiver(post_migrate)
@@ -36,7 +36,10 @@ def assign_permissions_to_groups(sender, **kwargs):
 
 
 def assign_apps_permissions_to_group(group_name: str, apps_permissions: Dict):
-    group, _ = Group.objects.get_or_create(name=group_name)
+    group: Group
+    created: bool
+
+    group, created = Group.objects.get_or_create(name=group_name)
     group.permissions.clear()
 
     for app_label, app_permissions in apps_permissions.items():
@@ -46,16 +49,22 @@ def assign_apps_permissions_to_group(group_name: str, apps_permissions: Dict):
             print(f"App '{app_label}' not found.")
 
 
-def assign_permission_to_app_models(group, app_label: str, perm_codenames: List[str]):
-    models = apps.get_app_config(app_label).get_models()
+def assign_permission_to_app_models(
+    group: Group,
+    app_label: str,
+    perm_codenames: List[str]
+):
+    models: Iterator[Model] = apps.get_app_config(app_label).get_models()
 
     for model in models:
-        content_type = ContentType.objects.get_for_model(model)
+        content_type: ContentType = ContentType.objects.get_for_model(model)
 
         for codename in perm_codenames:
             try:
-                permission = Permission.objects.get(codename=codename, content_type
-                =content_type)
+                permission: Permission = Permission.objects.get(
+                    codename=codename,
+                    content_type
+                    =content_type)
                 group.permissions.add(permission)
             except Permission.DoesNotExist:
                 print(
