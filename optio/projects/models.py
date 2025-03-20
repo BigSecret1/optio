063b33e1677e00adb04c25c55b16146e68afe432
BigSecret1 <dk2020mailaddress@gmail.com>
2025-03-20 10:40:25 +0530

    Sync Projects model with DB through migrations.

--- optio/projects/models.py
+++ optio/projects/models.py
@@ -2,24 +2,17 @@ from django.db import models
 
 
 class Project(models.Model):
-    id = models.AutoField(primary_key=True)  # Maps to the serial type in PostgreSQL
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    project_updated = models.DateTimeField(
-        auto_now=True)  # Maps to CURRENT_TIMESTAMP with timezone
+    project_updated = models.DateTimeField(auto_now=True)
     stars = models.IntegerField(default=0)
     project_description = models.TextField(blank=True, null=True)
     is_starred = models.BooleanField(default=False)
 
     class Meta:
         db_table = "projects"
-        indexes = [
-            models.Index(fields=["id"], name="projects_pkey"),
-            # Matches the primary key index
-        ]
-        constraints = [
-            # Foreign key from tasks already defined in the Task model
-        ]
-        managed = False  # Since the table already exists
+        constraints = []
+        managed = True
 
     def __str__(self):
         return self.name
