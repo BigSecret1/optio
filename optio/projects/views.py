2ec72139bd5ddb3520333dff9f23ab359bd4773b
BigSecret1 <dk2020mailaddress@gmail.com>
2025-03-23 23:52:18 +0530

    Manage project db transaction from ORM, remove postgresql raw operations.

--- optio/projects/views.py
+++ optio/projects/views.py
@@ -1,14 +1,11 @@
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import ProjectSerializer
-from optio.utils.db_manager import create_connection, close_connection
 from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
+from optio.projects.models import Project
+from .serializers import ProjectSerializer
 import logging
-
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 class ProjectListView(APIView):
@@ -16,30 +13,16 @@ class ProjectListView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        conn, cur = create_connection()
-        cur.execute("SELECT * FROM projects")
-        projects = cur.fetchall()
-
-        # Convert list of tuples to list of dictionaries
-        project_list = [
-            {'id': p['id'], 'name': p['name'], 'last_updated': p['project_updated'],
-             'stars': p['stars'], 'description': p['project_description']} for p in
-            projects]
-        close_connection(conn, cur)
-        logging.info("PROJECT LIST : %s", project_list)
-        serializer = ProjectSerializer(project_list, many=True)
+        projects = Project.objects.all()
+        serializer = ProjectSerializer(projects, many=True)
+        logging.info("PROJECT LIST : %s", serializer.data)
         return Response(serializer.data)
 
     def post(self, request):
         serializer = ProjectSerializer(data=request.data)
         if serializer.is_valid():
-            name = serializer.validated_data['name']
-            conn, cur = create_connection()
-            cur.execute("INSERT INTO projects (name) VALUES (%s) RETURNING id", (name,))
-            project_id = cur.fetchone()['id']
-            conn.commit()
-            close_connection(conn, cur)
-            return Response({'id': project_id, 'name': name},
+            project = serializer.save()
+            return Response({'id': project.id, 'name': project.name},
                             status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -49,67 +32,22 @@ class ProjectDetailView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, pk):
-        conn, cur = create_connection()
-        cur.execute("SELECT id, name FROM projects WHERE id=%s", (pk,))
-        project = cur.fetchone()
-        close_connection(conn, cur)
-        if project is None:
+        try:
+            project = Project.objects.get(pk=pk)
+            serializer = ProjectSerializer(project)
+            return Response(serializer.data)
+        except Project.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        serializer = ProjectSerializer({'id': project['id'], 'name': project['name']})
-        return Response(serializer.data)
 
     def put(self, request, pk):
-        serializer = ProjectSerializer(data=request.data)
+        try:
+            project = Project.objects.get(pk=pk)
+        except Project.DoesNotExist:
+            return Response({'error': 'Project not found'},
+                            status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ProjectSerializer(project, data=request.data)
         if serializer.is_valid():
-            name = serializer.validated_data['name']
-            conn, cur = create_connection()
-            cur.execute("UPDATE projects SET name=%s WHERE id=%s", (name, pk))
-            conn.commit()
-            close_connection(conn, cur)
-            return Response({'id': pk, 'name': name})
+            serializer.save()
+            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        conn, cur = create_connection()
-        delete_table_entities = DeleteTableEntities()
-
-        try:
-            # Find the references in all tables and then delete project
-            cur.execute("SELECT id FROM tasks WHERE project_id=%s", (pk,))
-            tasks_id = cur.fetchall()
-            tasks_id = [task_id['id'] for task_id in tasks_id]
-            delete_table_entities.delete_task_status_history_references(tasks_id)
-            delete_table_entities.delete_tasks(pk)
-
-            cur.execute("DELETE FROM projects WHERE id=%s", (pk,))
-            conn.commit()
-            close_connection(conn, cur)
-            return Response("Deleted the project and it's content successfully",
-                            status=status.HTTP_204_NO_CONTENT)
-        except Exception as err:
-            return Response(f"An error occured while deleting the project {err}")
-
-
-class DeleteTableEntities:
-
-    def delete_tasks(self, project_id):
-        try:
-            conn, cur = create_connection()
-            cur.execute("DELETE FROM tasks WHERE project_id=%s", (project_id,))
-            conn.commit()
-            close_connection(conn, cur)
-        except Exception as err:
-            return Response(f"An error occured while deleting tasks {err}")
-
-    def delete_task_status_history_references(self, tasks_id):
-        try:
-            conn, cur = create_connection()
-            logging.info("RECIVED IDS ARE : ", tasks_id)
-            for task_id in tasks_id:
-                cur.execute("DELETE FROM task_status_history WHERE task_id=%s",
-                            (task_id,))
-                conn.commit()
-            close_connection(conn, cur)
-        except Exception as err:
-            return Response(
-                f"An error occured while deleting task status history {err}")
