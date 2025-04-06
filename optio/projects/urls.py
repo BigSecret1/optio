15b43f7a941b6662c370f5663510b3a60069f5c4
Dinesh Kumar <dk2020mailaddress@gmail.com>
2025-04-06 12:19:07 +0530

    Add Some test cases for view classes. (#32)

--- optio/projects/urls.py
+++ optio/projects/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ProjectListView, ProjectDetailView
+from .views import ProjectListView, ProjectDetailView, CreateProjectAPIView
 
 urlpatterns = [
-    path('list/', ProjectListView.as_view(), name='project_list'),
-    path('detail/<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
+    path("list/", ProjectListView.as_view(), name='project-list'),
+    path("detail/<int:pk>/", ProjectDetailView.as_view(), name='project-detail'),
+    path("create/", CreateProjectAPIView.as_view(), name='create-project'),
+
 ]
