b7470cd85338efe66c63fabc5cb4f62e5197cf8b
BigSecret1 <dk2020mailaddress@gmail.com>
2025-04-14 19:13:24 +0530

    Rename root app directory.

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="optio",  # Replace with your project name
+    name="optio",  
     version="0.1.0",
-    packages=find_packages(),  # Automatically find packages
-    install_requires=[
-        # List dependencies here
-    ],
+    packages=find_packages(),  
+    install_requires=[],
 )
