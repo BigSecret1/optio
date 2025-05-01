9eb03ba49234ba336f224a78688fb470a2117499
Dinesh Kumar <dk2020mailaddress@gmail.com>
2025-05-01 17:47:58 +0530

    Add user interface to list all users by Admin

---
+++ optio-web/src/components/auth/ListUsers.js
@@ -0,0 +1,106 @@
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  IconButton,
+  Typography,
+  Button,
+} from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
+
+import "./styles/list-users.css";
+
+const users = [
+  {
+    id: 1,
+    firstName: "anyusernamewhichislong",
+    lastName: "Doe",
+    username: "johndoe",
+    isActive: true,
+    role: "Admin",
+  },
+  {
+    id: 2,
+    firstName: "Jane",
+    lastName: "Iamalonglastname",
+    username: "janesmith",
+    isActive: false,
+    role: "User",
+  },
+];
+
+function ListUsers() {
+  function handleEdit(id) {
+    console.log("Edit user with id:", id);
+  }
+
+  function handleDelete(id) {
+    console.log("Delete user with id:", id);
+  }
+
+  return (
+    <>
+      <Button sx={{ backgroundColor: "#304971", color: "white" }}>
+        ADD USER
+      </Button>
+      <TableContainer
+        component={Paper}
+        sx={{ mt: 4, backgroundColor: "#304971", color: "white" }}
+      >
+        <Typography variant="h6" sx={{ p: 2 }}>
+          List Users
+        </Typography>
+        <Table className="user-table">
+          <TableHead>
+            <TableRow>
+              <TableCell></TableCell>
+              <TableCell>First Name</TableCell>
+              <TableCell>Last Name</TableCell>
+              <TableCell>Username</TableCell>
+              <TableCell>Is Active</TableCell>
+              <TableCell>Role</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users.map((user) => (
+              <TableRow key={user.id}>
+                <TableCell className="icon-cell">
+                  <IconButton
+                    className="icon-button"
+                    color="primary"
+                    size="small"
+                    onClick={() => handleEdit(user.id)}
+                  >
+                    <FontAwesomeIcon icon={faEdit} className="fa-icon" />
+                  </IconButton>
+                  <IconButton
+                    className="icon-button"
+                    color="error"
+                    size="small"
+                    onClick={() => handleDelete(user.id)}
+                  >
+                    <FontAwesomeIcon icon={faTrash} className="fa-icon" />
+                  </IconButton>
+                </TableCell>
+
+                <TableCell>{user.firstName}</TableCell>
+                <TableCell>{user.lastName}</TableCell>
+                <TableCell>{user.username}</TableCell>
+                <TableCell>{user.isActive ? "Yes" : "No"}</TableCell>
+                <TableCell>{user.role}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+}
+
+export default ListUsers;
