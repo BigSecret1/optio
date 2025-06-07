2c3583170d5907664466114bab9bb8fee5d95bfd
BigSecret1 <dk2020mailaddress@gmail.com>
2025-06-07 10:02:54 +0530

    Remove migration connectors as profile migrations are causing problem.

--- optio/users/apps.py
+++ optio/users/apps.py
@@ -10,6 +10,7 @@ class ProfilesConfig(AppConfig):
     def ready(self):
         """Trigger signal post migration to create groups and assign permissions to groups"""
 
-        from optio.users.signals import create_groups, assign_permissions_to_groups
-        post_migrate.connect(create_groups, sender=self.__class__)
-        post_migrate.connect(assign_permissions_to_groups, sender=self.__class__)
+        # from optio.users.signals import create_groups, assign_permissions_to_groups
+        # post_migrate.connect(create_groups, sender=self.__class__)
+        # post_migrate.connect(assign_permissions_to_groups, sender=self.__class__)
+        import optio.users.signals
