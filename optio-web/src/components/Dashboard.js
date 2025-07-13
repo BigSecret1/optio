dd590c15e7aae17222193638668e43366312aa03
Dinesh Kumar <dk2020mailaddress@gmail.com>
2025-07-13 11:10:38 +0530

    Add routes to dashboard cards on click.

--- optio-web/src/components/Dashboard.js
+++ optio-web/src/components/Dashboard.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import Card from './Card';
+import React from "react";
+import Card from "./Card";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const Dashboard = () => {
+import "./dashboard.css";
+
+function Dashboard() {
+  const navigate = useNavigate();
+
   const cards = [
-    { title: "Projects", text: "total number of projects : x" },
-    { title: "Tasks", text: "14 completed  12 running 12 pending tasks: 18" },
-    { title: "Performance", text: "completed : 20%" },
+    { title: "Projects", text: "", path: "/projects/list" },
+    { title: "Tasks", text: "", path: "/tasks" },
+    { title: "Quick notes", text: "", path: "/quicknote" },
   ];
 
+  const handleCardClick = (path) => {
+    navigate(path);
+  };
+
   return (
-    <div className="container">
+    <div className="dashboard-container">
       <div className="row">
         {cards.map((card, index) => (
-          <div className="col-md-4 mb-3" key={index}>
+          <div
+            className="col-md-4 mb-3"
+            key={index}
+            style={{ cursor: "pointer" }}
+            onClick={() => handleCardClick(card.path)}
+          >
             <Card title={card.title} text={card.text} />
           </div>
         ))}
       </div>
     </div>
   );
-};
+}
 
 export default Dashboard;
