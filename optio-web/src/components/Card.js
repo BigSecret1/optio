382fed946460876661cbbfca82452ddd6506761e
BigSecret1 <dk2020mailaddress@gmail.com>
2025-07-13 11:05:42 +0530

    Add route to dashboard cards.

--- optio-web/src/components/Card.js
+++ optio-web/src/components/Card.js
@@ -1,16 +1,15 @@
+import React from "react";
+import "./Card.css";
 
-import React from 'react';
-import './Card.css';
-
-const Card = ({ title, text }) => {
+function Card({ title, text }) {
   return (
-    <div className="card custom-card" >
+    <div className="card custom-card">
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
         <p className="card-text">{text}</p>
       </div>
     </div>
   );
-};
+}
 
 export default Card;
